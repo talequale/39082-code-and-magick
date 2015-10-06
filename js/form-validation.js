dce0f75a1691baa0137691e6b0ba7fc24b55a43f
Юлия Антонова <antonova.julia@gmail.com>
2015-10-06 21:34:25 +0300

    правки (трэвис)

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,11 +1,12 @@
+'use strict';
 (function() {
   var formElement = document.forms[1];
-  var userName = formElement["review-name"];
-  var message = formElement["review-text"];
-  var nameTip = document.querySelector(".review-fields-name");
-  var messageTip = document.querySelector(".review-fields-text");
-  var tips = document.querySelector(".review-fields");
-  var FIELDS_TO_PERSIST = ["review-name", "review-mark"];
+  var userName = formElement['review-name'];
+  var message = formElement['review-text'];
+  var nameTip = document.querySelector('.review-fields-name');
+  var messageTip = document.querySelector('.review-fields-text');
+  var tips = document.querySelector('.review-fields');
+  var FIELDS_TO_PERSIST = ['review-name', 'review-mark'];
   var lastBirthday = new Date(2014, 10, 11);
   var now = new Date();
   var millisecondsSince1970 = now.getTime();
@@ -18,33 +19,33 @@
   function hideTip() {
     for (var i = 0; i < FIELDS_TO_VALIDATE.length; i++) {
       if (FIELDS_TO_VALIDATE[i].value.trim().length) {
-        TIPS[i].classList.add("invisible")
-      } else if (TIPS[i].classList.contains("invisible")) {
-        TIPS[i].classList.remove("invisible")
+        TIPS[i].classList.add('invisible');
+      } else if (TIPS[i].classList.contains('invisible')) {
+        TIPS[i].classList.remove('invisible');
       }
 
-      if (nameTip.classList.contains("invisible") && messageTip.classList.contains("invisible")) {
-        tips.classList.add("invisible");
+      if (nameTip.classList.contains('invisible') && messageTip.classList.contains('invisible')) {
+        tips.classList.add('invisible');
       } else {
-        tips.classList.remove("invisible")
+        tips.classList.remove('invisible');
       }
     }
   }
 
-  //восстановление значений из куки
+  // восстановление значений из куки
   function restoreFormValueFromCookies() {
     for (var i = 0; i < FIELDS_TO_PERSIST.length; i++) {
-      formElement[FIELDS_TO_PERSIST[i]].value = docCookies.getItem(FIELDS_TO_PERSIST[i])
+      formElement[FIELDS_TO_PERSIST[i]].value = docCookies.getItem(FIELDS_TO_PERSIST[i]);
     }
   }
 
-  //отправка формы
+  // отправка формы
   function postReview(event) {
     if ((userName.value.trim().length) && (message.value.trim().length)) {
       event.preventDefault();
       for (var i = 0; i < FIELDS_TO_PERSIST.length; i++) {
-        docCookies.setItem(FIELDS_TO_PERSIST[i], formElement[FIELDS_TO_PERSIST[i]].value, new Date(expireDate))
-      };
+        docCookies.setItem(FIELDS_TO_PERSIST[i], formElement[FIELDS_TO_PERSIST[i]].value, new Date(expireDate));
+      }
     } else {
       event.preventDefault();
     }
@@ -54,7 +55,7 @@
   hideTip();
   for (var i = 0; i < FIELDS_TO_VALIDATE.length; i++) {
     FIELDS_TO_VALIDATE[i].onchange = hideTip;
-  };
+  }
   formElement.onsubmit = postReview;
 
 })();
--- js/reviews.js
+++ js/reviews.js
@@ -1,76 +1,76 @@
-"use strict";
+'use strict';
 
 (function() {
 
   var readyState = {
-    "UNSENT": 0,
-    "OPENED": 1,
-    "HEADERS_RECEIVED": 2,
-    "LOADING": 3,
-    "DONE": 4
+    'UNSENT': 0,
+    'OPENED': 1,
+    'HEADERS_RECEIVED': 2,
+    'LOADING': 3,
+    'DONE': 4
   };
 
   var ratingClassName = {
-    "1": "review-rating-one",
-    "2": "review-rating-two",
-    "3": "review-rating-three",
-    "4": "review-rating-four",
-    "5": "review-rating-five"
+    '1': 'review-rating-one',
+    '2': 'review-rating-two',
+    '3': 'review-rating-three',
+    '4': 'review-rating-four',
+    '5': 'review-rating-five'
   };
 
   var REQUEST_FAILURE_TIMEOUT = 10000;
-  var reviewsFilters = document.querySelector(".reviews-filter");
-  var reviewsContainer = document.querySelector(".reviews-list");
+  var reviewsFilters = document.querySelector('.reviews-filter');
+  var reviewsContainer = document.querySelector('.reviews-list');
   var reviews;
 
   function renderReviews(reviews) {
-    reviewsContainer.classList.remove("reviews-load-failure");
-    reviewsContainer.innerHTML = "";
+    reviewsContainer.classList.remove('reviews-load-failure');
+    reviewsContainer.innerHTML = '';
 
-    var reviewTemplate = document.getElementById("review-template");
+    var reviewTemplate = document.getElementById('review-template');
     var reviewsFragment = document.createDocumentFragment();
 
-    reviewsFilters.classList.add("invisible");
+    reviewsFilters.classList.add('invisible');
 
-    reviews.forEach(function(review, i) {
+    reviews.forEach(function(review) {
       var newReviewElement = reviewTemplate.content.children[0].cloneNode(true);
-      var avatarTemplate = newReviewElement.querySelector(".review-author");
+      var avatarTemplate = newReviewElement.querySelector('.review-author');
       avatarTemplate.title = review.author.name;
-      newReviewElement.querySelector(".review-text").textContent = review.description;
-      newReviewElement.querySelector(".review-rating").classList.add(ratingClassName[Math.floor(review.rating)]);
+      newReviewElement.querySelector('.review-text').textContent = review.description;
+      newReviewElement.querySelector('.review-rating').classList.add(ratingClassName[Math.floor(review.rating)]);
 
       if (review.author.picture) {
         var reviewAvatar = new Image();
         reviewAvatar.src = review.author.picture;
         var imageLoadTimeout = setTimeout(function() {
-          newReviewElement.classList.add("review-load-failure");
+          newReviewElement.classList.add('review-load-failure');
         }, REQUEST_FAILURE_TIMEOUT);
         reviewAvatar.onload = function() {
-          reviewAvatar.classList.add("review-author");
+          reviewAvatar.classList.add('review-author');
           reviewAvatar.title = review.author.name;
-          reviewAvatar.style.width = "124px";
-          reviewAvatar.style.height = "124px";
+          reviewAvatar.style.width = '124px';
+          reviewAvatar.style.height = '124px';
           newReviewElement.replaceChild(reviewAvatar, avatarTemplate);
           clearTimeout(imageLoadTimeout);
         };
         reviewAvatar.onerror = function() {
-          newReviewElement.classList.add("review-load-failure");
-        }
+          newReviewElement.classList.add('review-load-failure');
+        };
       }
       reviewsFragment.appendChild(newReviewElement);
     });
     reviewsContainer.appendChild(reviewsFragment);
-    reviewsFilters.classList.remove("invisible");
+    reviewsFilters.classList.remove('invisible');
   }
 
   function showLoadFailure() {
-    reviewsContainer.classList.add("reviews-load-failure");
+    reviewsContainer.classList.add('reviews-load-failure');
   }
 
   function loadReviews(callback) {
     var xhr = new XMLHttpRequest();
     xhr.timeout = REQUEST_FAILURE_TIMEOUT;
-    xhr.open("get", "data/reviews.json");
+    xhr.open('get', 'data/reviews.json');
     xhr.send();
 
     xhr.onreadystatechange = function(evt) {
@@ -80,13 +80,13 @@
         case readyState.OPENED:
         case readyState.HEADERS_RECEIVED:
         case readyState.LOADING:
-          reviewsContainer.classList.add("reviews-list-loading");
+          reviewsContainer.classList.add('reviews-list-loading');
           break;
         case readyState.DONE:
         default:
-          if (loadedXhr.status == 200) {
+          if (loadedXhr.status === 200) {
             var data = loadedXhr.response;
-            reviewsContainer.classList.remove("reviews-list-loading");
+            reviewsContainer.classList.remove('reviews-list-loading');
             callback(JSON.parse(data));
           }
           if (loadedXhr.status > 400) {
@@ -94,7 +94,7 @@
           }
       }
     };
-    xhr.ontimeout = function () {
+    xhr.ontimeout = function() {
       showLoadFailure();
     };
   }
@@ -104,7 +104,7 @@
 
     switch (filterName) {
 
-      case "reviews-recent":
+      case 'reviews-recent':
         filteredReviews = filteredReviews.sort(function (a, b) {
           if (new Date(a.date) > new Date(b.date)) {
             return -1;
@@ -118,10 +118,10 @@
         });
         break;
 
-      case "reviews-good":
-        filteredReviews = filteredReviews.filter(function (item) {
+      case 'reviews-good':
+        filteredReviews = filteredReviews.filter(function(item) {
           return item.rating > 2;
-        }).sort(function (a, b) {
+        }).sort(function(a, b) {
           if (a.rating > b.rating) {
             return -1;
           }
@@ -136,10 +136,10 @@
         });
         break;
 
-      case "reviews-bad":
-        filteredReviews = filteredReviews.filter(function (item) {
+      case 'reviews-bad':
+        filteredReviews = filteredReviews.filter(function(item) {
           return item.rating < 3;
-        }).sort(function (a, b) {
+        }).sort(function(a, b) {
           if (a.rating < b.rating) {
             return -1;
           }
@@ -152,15 +152,15 @@
         });
         break;
 
-      case "reviews-popular":
-        filteredReviews = filteredReviews.sort(function (a, b) {
-          if (a["review-rating"] > b["review-rating"]) {
+      case 'reviews-popular':
+        filteredReviews = filteredReviews.sort(function(a, b) {
+          if (a['review-rating'] > b['review-rating']) {
             return -1;
           }
-          if (a["review-rating"] < b["review-rating"]) {
+          if (a['review-rating'] < b['review-rating']) {
             return 1;
           }
-          if (a["review-rating"] === b["review-rating"]) {
+          if (a['review-rating'] === b['review-rating']) {
             return 0;
           }
         });
@@ -179,18 +179,18 @@
   }
 
   function initFilters() {
-    var filterButtons = document.querySelectorAll(".reviews-filter-item");
+    var filterButtons = document.querySelectorAll('.reviews-filter-item');
     for (var i = 0, l = filterButtons.length; i < l; i++) {
-      filterButtons[i].onclick = function (event) {
+      filterButtons[i].onclick = function(event) {
         var checkedButton = event.currentTarget;
         setActiveFilter(checkedButton.htmlFor);
-      }
+      };
     }
   }
 
   initFilters();
   loadReviews(function(loadedReviews) {
     reviews = loadedReviews;
-    setActiveFilter("sort-hotels-default");
+    setActiveFilter('sort-hotels-default');
   });
 })();
