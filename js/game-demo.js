82b4d4644de7b144662a1924f5c9fdb452810c5a
Юлия Антонова <antonova.julia@gmail.com>
2015-10-09 20:45:16 +0300

    исправила часть ошибок

--- js/game-demo.js
+++ js/game-demo.js
@@ -1,46 +1,45 @@
-/*'use strict';*/
-/*(function() {*/
+'use strict';
+(function() {
   var cloudBox = document.querySelector('.header-clouds');
   var xValue = 50;
   var delta; // переменная, в которую записывается разница значений скролла
   var movingClouds = document.querySelector('.header-clouds');
 
   // определение направления скролла
-  var scrollDirection = function() {
+  function scrollDirection() {
     var pixelsFromTop = window.pageYOffset || document.documentElement.scrollTop;
     window.addEventListener('scroll', function() {
       var currentPixelsFromTop = window.pageYOffset || document.documentElement.scrollTop;
       if (currentPixelsFromTop > pixelsFromTop) {
-        return delta = 1;
+        delta = 1;
       } else if (currentPixelsFromTop < pixelsFromTop) {
-        return delta = -1;
+        delta = -1;
       }
+      return delta;
     });
-  };
+  }
 
   // видимость блока с облаками
   function elementVisibilityCheck(element) {
     var bottomValue = element.getBoundingClientRect().bottom;
     if (bottomValue < 0) {
-      window.dispatchEvent(new CustomEvent('stopParallax'))
+      window.dispatchEvent(new CustomEvent('stopParallax'));
     } else {
-      window.dispatchEvent(new CustomEvent('startParallax'))
+      window.dispatchEvent(new CustomEvent('startParallax'));
     }
   }
 
   function scrollInit() {
     var someTimeout;
-    var moveBackground = function() {
+    function moveBackground() {
       scrollDirection();
-      movingClouds.style.backgroundPosition = xValue + "%" + " " + "0%";
+      movingClouds.style.backgroundPosition = xValue + '%' + ' ' + '0%';
       if (delta < 0) {
         xValue = xValue + 1;
-        console.log("+1");
       } else {
         xValue = xValue - 1;
-        console.log("-1");
       }
-    };
+    }
     window.addEventListener('scroll', moveBackground);
     window.addEventListener('scroll', function() {
       clearTimeout(someTimeout);
@@ -51,8 +50,8 @@
       window.addEventListener('startParallax', function() {
         window.addEventListener('scroll', moveBackground);
       })
-    })
+    });
   }
 
   scrollInit();
-/*})();*/
+})();
